e5745b22e6f4f61c09f90644a875857a61be4ef9
Tyler Jewell tjewell@codenvy.com <tjewell@codenvy.com>
2017-02-01 12:11:56 -0800

    initial
    Signed-off-by: Tyler Jewell tjewell@codenvy.com <tjewell@codenvy.com>

--- dockerfiles/cli/test.sh
+++ dockerfiles/cli/test.sh
@@ -10,7 +10,9 @@
 
 BATS_BASE_DIR=$(cd "$(dirname "$0")"/..; pwd)
 . $BATS_BASE_DIR/build.include
+BATS_BASE_DIR=$(get_mount_path $BATS_BASE_DIR)
 
+echo $BATS_BASE_DIR
 init "$@"
 IMAGE_NAME="eclipse/che-bats:$TAG"
 
@@ -19,8 +21,14 @@ IMAGE_NAME="eclipse/che-bats:$TAG"
 #   The file has to be placed in tests folder in directory containing this script
 # (Optional) second argument is options for a docker run command.
 run_test_in_docker_container() {
-  docker run $2 -v $BATS_BASE_DIR:$BATS_BASE_DIR -e CLI_IMAGE_TAG=$TAG -e BATS_BASE_DIR=$BATS_BASE_DIR -v /var/run/docker.sock:/var/run/docker.sock $IMAGE_NAME bats $BATS_BASE_DIR/cli/tests/$1
+  docker_exec run $2 \
+       -v $BATS_BASE_DIR:$BATS_BASE_DIR \
+       -e CLI_IMAGE_TAG=$TAG \
+       -e BATS_BASE_DIR="${BATS_BASE_DIR}" \
+       -v /var/run/docker.sock:/var/run/docker.sock \
+           $IMAGE_NAME bats "${BATS_BASE_DIR}"/cli/tests/$1
 }
+
 
 echo "Running tests in container from image $IMAGE_NAME"
 echo "Running functional bats tests for CLI prompts and usage"
